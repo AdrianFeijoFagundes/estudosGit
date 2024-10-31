7b5618a3a1c41d15acbde663e90f21bab4f388f2
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2024-10-30 21:19:22 -0300

    CREATE: menu console interface

--- calculadora.js
+++ calculadora.js
@@ -4,3 +4,10 @@ const calculadora = {
     mul: (n1, n2) => n1 * n2,
     div: (n1, n2) => n1 / n2
 }
+
+const menu = () => {
+    console.log("1 - Soma")
+    console.log("2 - Divisão")
+    console.log("3 - Multiplicação")
+    console.log("4 - Divisão")
+}
